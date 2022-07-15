5ef8dc9b7a8371b7c5473997dea29b2d310d3cfa
polt0s <poltos.pro@gmail.com>
2022-07-15 16:06:07 +0300

    Update component Button

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import type { ButtonHTMLAttributes } from 'react';
+import cn from 'classnames';
 
-export const Button = () => {
+import styles from './Button.module.css';
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+    colorText?: 'white' | 'black' | 'blue';
+    background?: 'blue' | 'red' | 'grey' | 'green' | 'white';
+    isLoading?: boolean;
+}
+
+export const Button = ({ colorText, background, className, ...rest }: IButton) => {
     return (
-        <button>
-            BUTTON
+        <button
+            type="button"
+            className={cn(
+                styles['Button'],
+                styles[`Background-${background}`],
+                styles[`Color-${colorText}`],
+                className
+            )}
+            {...rest}
+        >
+            {rest.children}
         </button>
     );
 };
