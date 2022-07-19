60e15d05021e5135bd438a2c650b3908471ab730
polt0s <poltos.pro@gmail.com>
2022-07-20 01:02:16 +0300

    Update conponent Button

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,27 +1,34 @@
-import type { ButtonHTMLAttributes } from 'react';
+import React from 'react';
 import cn from 'classnames';
+
+import type { ButtonHTMLAttributes } from 'react';
 
 import styles from './Button.module.css';
 
-interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface IButton extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled'> {
     colorText?: 'white' | 'black' | 'blue';
     background?: 'blue' | 'red' | 'grey' | 'green' | 'white';
     isLoading?: boolean;
+    width?: string;
+    disabled?: boolean;
 }
 
-export const Button = ({ colorText, background, className, ...rest }: IButton) => {
+export const Button = React.memo(({ colorText, background, className, width, disabled, ...rest }: IButton) => {
     return (
         <button
             type="button"
             className={cn(
                 styles['Button'],
+                disabled ? styles['Background-disabled'] : null,
                 styles[`Background-${background}`],
                 styles[`Color-${colorText}`],
                 className
             )}
+            style={{ width: width, ...rest.style }}
+            disabled={disabled}
             {...rest}
         >
             {rest.children}
         </button>
     );
-};
+});
