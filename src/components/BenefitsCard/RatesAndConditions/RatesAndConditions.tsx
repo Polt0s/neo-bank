6041eece8cba34685aa5ae075ab981d16442393a
polt0s <poltos.pro@gmail.com>
2022-07-30 00:25:06 +0300

    Create a new component RatesAndConditions

---
+++ src/components/BenefitsCard/RatesAndConditions/RatesAndConditions.tsx
@@ -0,0 +1,31 @@
+import { Box, Text } from '@chakra-ui/react';
+
+import styles from './RatesAndConditions.module.css';
+
+interface IRatesAndConditions {
+    items: TDataRatesAndConditions[];
+}
+
+export const RatesAndConditions = ({ items }: IRatesAndConditions) => (
+    <Box padding="2rem 0">
+        <table className={styles['Table']}>
+            <tbody>
+                {items.map((item, index) => (
+                    <tr className={index !== 0 && styles['Table__tr']}>
+                        <td style={{ padding: '24px 32px 25px 0px' }}>
+                            <Text>{item.header}</Text>
+                        </td>
+                        <td>
+                            <Text className={styles['Table__rd-text']}>{item.description}</Text>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </Box>
+);
+
+export type TDataRatesAndConditions = {
+    header: string;
+    description: string;
+}
