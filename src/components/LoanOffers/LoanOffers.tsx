d714f1690aa1a80275c299d05d9ab422ad82e0ef
polt0s <poltos.pro@gmail.com>
2022-08-03 09:18:29 +0300

    Create a new component LoanOffers

---
+++ src/components/LoanOffers/LoanOffers.tsx
@@ -0,0 +1,92 @@
+import { Flex, Heading, Text, Center, Box } from '@chakra-ui/react';
+
+import {
+    Button,
+    Card,
+    CompleteIcon,
+    RejectIcon
+} from 'shared';
+
+import SurpriseImage from 'assets/image/SurpriseImage.png';
+
+interface ILoanOffers {
+    data: TDataOffers[];
+}
+
+export const LoanOffers = ({ data }: ILoanOffers) => {
+    return (
+        <Box marginBottom="200px">
+            <Heading size="lg" marginBottom="2rem" textAlign="center">Your personal offer</Heading>
+
+            <Flex gap={10}>
+                {data.map((offer, index) => (
+                    <Card key={index}>
+                        <Center marginBottom="2rem">
+                            <img src={SurpriseImage} alt={SurpriseImage} width="150px" height="150px" />
+                        </Center>
+
+                        <Flex
+                            gap={3}
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="center"
+                            marginBottom="4rem"
+                        >
+                            <Text>Requested amount: {offer.requestedAmount.toLocaleString('ru')}</Text>
+                            <Text>Total amount: {offer.totalAmount.toLocaleString('ru')}</Text>
+                            <Text>For {offer.term} months</Text>
+                            <Text>Monthly payment: {offer.monthlyPayment.toLocaleString('ru')}</Text>
+                            <Text>Your rate: {offer.rate}%</Text>
+
+                            {offer.isInsuranceEnabled ? (
+                                <Flex gap={2}>
+                                    <Text>Insurance included</Text>
+                                    <CompleteIcon />
+                                </Flex>
+                            ) : (
+                                <Flex gap={2}>
+                                    <Text>Insurance included</Text>
+                                    <RejectIcon />
+                                </Flex>
+                            )}
+
+                            {offer.isSalaryClient ? (
+                                <Flex gap={2}>
+                                    <Text>Salary client</Text>
+                                    <CompleteIcon />
+                                </Flex>
+                            ) : (
+                                <Flex gap={10}>
+                                    <Text>Salary client</Text>
+                                    <RejectIcon />
+                                </Flex>
+                            )}
+                        </Flex>
+
+                        <Center>
+                            <Button
+                                width="9.3rem"
+                                height="3rem"
+                                background="blue"
+                                colorText="white"
+                            >
+                                Select
+                            </Button>
+                        </Center>
+                    </Card>
+                ))}
+            </Flex>
+        </Box>
+    );
+};
+
+type TDataOffers = {
+    applicationId: number;
+    requestedAmount: number; // запрашиваемая сумма
+    totalAmount: number; // итоговая сумма
+    term: number; // количество месяцев
+    monthlyPayment: number; // ежемесячный платёж
+    rate: number; // ставка
+    isInsuranceEnabled: boolean; // будет ли страховка
+    isSalaryClient: boolean; // зарплатный ли клиент
+}
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,5 @@
 export { HeaderContainer } from './HeaderContainer';
 export { FormApplicationPrescoringStepContainer } from './FormApplicationPrescoringStepContainer';
 export { CurrencyDisplayContainer } from './CurrencyDisplayContainer';
+export { FormApplicationScoringStepContainer } from './FormApplicationScoringStepContainer';
+export { LoanOffersContainer } from './LoanOffersContainer';
