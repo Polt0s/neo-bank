06bab6fad85afbafb6f2245a9d8b292f4dffa058
polt0s <poltos.pro@gmail.com>
2022-08-22 17:35:59 +0300

    Update shared component Icons

--- src/shared/Icons/Icons.tsx
+++ src/shared/Icons/Icons.tsx
@@ -158,6 +158,27 @@ const Email = ({ color }: IIcons) => (
     </svg>
 );
 
+const Sun = ({ color }: IIcons) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none">
+        <circle cx="12" cy="12" r="3" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" />
+        <path d="M12 5V3" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M12 21V19" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M16.9498 7.05026L18.364 5.63605" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M5.63608 18.364L7.05029 16.9498" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M19 12L21 12" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M3 12L5 12" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M16.9498 16.9497L18.364 18.364" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+        <path d="M5.63608 5.63602L7.05029 7.05023" stroke={iconsConfig.colors[color || 'default']} strokeWidth="2" strokeLinecap="round" />
+    </svg>
+);
+
+const Moon = ({ color }: IIcons) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none">
+        <path d="M16.1637 18.5102L17.0356 18.0205L16.1637 18.5102ZM15.5781 19.9582L15.2918 19L15.5781 19.9582ZM15.4389 6.249L16.1341 6.9678L15.4389 6.249ZM16.1637 5.48976L17.0356 5.97952L16.1637 5.48976ZM15.5781 4.04183L15.2918 5L15.5781 4.04183ZM14 12C14 10.0246 14.817 8.24172 16.1341 6.9678L14.7437 5.5302C13.0531 7.16533 12 9.46056 12 12H14ZM16.1341 17.0322C14.817 15.7583 14 13.9754 14 12H12C12 14.5394 13.0531 16.8347 14.7437 18.4698L16.1341 17.0322ZM15 19C11.134 19 8 15.866 8 12H6C6 16.9706 10.0294 21 15 21V19ZM8 12C8 8.13401 11.134 5 15 5V3C10.0294 3 6 7.02944 6 12H8ZM14.7437 18.4698C14.9664 18.6852 15.1162 18.8303 15.2206 18.9377C15.2719 18.9905 15.3012 19.0229 15.3168 19.0415C15.3356 19.0638 15.3179 19.0463 15.2918 19L17.0356 18.0205C16.9274 17.828 16.7656 17.6578 16.6549 17.5438C16.5236 17.4088 16.3468 17.238 16.1341 17.0322L14.7437 18.4698ZM15 21C15.2361 21 15.5523 21.0095 15.8643 20.9163L15.2918 19C15.3199 18.9916 15.3307 18.994 15.2883 18.9964C15.2332 18.9996 15.1569 19 15 19V21ZM15.2918 19L15.8643 20.9163C17.0923 20.5495 17.6633 19.1379 17.0356 18.0205L15.2918 19ZM16.1341 6.9678C16.3468 6.76203 16.5236 6.59121 16.6549 6.45617C16.7656 6.34225 16.9274 6.17203 17.0356 5.97952L15.2918 5C15.3179 4.95369 15.3356 4.93623 15.3168 4.95852C15.3012 4.97707 15.2719 5.0095 15.2206 5.06226C15.1162 5.16969 14.9664 5.31477 14.7437 5.5302L16.1341 6.9678ZM15 5C15.1569 5 15.2332 5.00038 15.2883 5.00356C15.3307 5.00601 15.3199 5.00838 15.2918 5L15.8643 3.08366C15.5523 2.9905 15.2361 3 15 3V5ZM17.0356 5.97952C17.6633 4.86207 17.0923 3.45049 15.8643 3.08366L15.2918 5L17.0356 5.97952Z"
+            fill={iconsConfig.colors[color || 'default']} />
+    </svg>
+);
+
 const CompleteIcon = () => IconHOC({}, Complete);
 const RejectIcon = () => IconHOC({ colors: 'redColor' }, Reject);
 const BagShopIcon = () => IconHOC({ colors: 'grayColor', size: 40 }, BagShop);
@@ -169,6 +190,8 @@ const PDFIcon = () => IconHOC({ colors: 'grayColor', size: 60 }, PDF);
 const BankIcon = () => IconHOC({ colors: 'blackSecondColor', size: 120 }, Bank);
 const SendLetterIcon = () => IconHOC({ colors: 'whiteColor', size: 40 }, SendLetter);
 const EmailIcon = () => IconHOC({ colors: 'violetColor', size: 40 }, Email);
+const SunIcon = () => IconHOC({ colors: 'blackColor' }, Sun);
+const MoonIcon = () => IconHOC({ colors: 'blackColor' }, Moon);
 
 export {
     CompleteIcon,
@@ -181,5 +204,7 @@ export {
     PDFIcon,
     BankIcon,
     SendLetterIcon,
-    EmailIcon
+    EmailIcon,
+    SunIcon,
+    MoonIcon
 };
