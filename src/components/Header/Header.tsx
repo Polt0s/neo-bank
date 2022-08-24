8ee052ee379d3ab6ce845cb600ecc77c936a3997
polt0s <poltos.pro@gmail.com>
2022-08-24 14:00:06 +0300

    Update module Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,46 +6,85 @@ import {
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-import { Button, Logo } from 'shared';
+import {
+    Button,
+    Logo,
+    MoonIcon,
+    SunIcon
+} from 'shared';
+
+import type { TColorMode } from 'context';
 
 import styles from './Header.module.css';
 
 interface IHeader {
     routesPaths: Record<string, string>;
     goBackHome: () => void;
+    toggleTheme: () => void;
+    stateTheme: TColorMode;
+    currentColorMode: 'lightColorMode' | 'darkColorMode';
 }
 
-export const Header = ({ routesPaths, goBackHome }: IHeader) => {
+export const Header = ({
+    routesPaths,
+    goBackHome,
+    toggleTheme,
+    stateTheme,
+    currentColorMode
+}: IHeader) => {
     return (
         <header className={styles['Header']}>
             <Logo onClick={goBackHome} />
-
             <Container maxW="md">
                 <Flex
                     alignItems="center"
                     gap="10"
                     justifyContent="center"
                 >
-                    <Link to={routesPaths['Loan']} className={styles.Header__link}>Credit card</Link>
-                    <Link to={routesPaths['Product']} className={styles.Header__link}>Product</Link>
-                    <Link to={routesPaths['Account']} className={styles.Header__link}>Account</Link>
-                    <Text className={styles.Header__link}>Resource</Text>
+                    <Link to={routesPaths['Loan']}>
+                        <Text className={styles.Header__link} color={stateTheme.color}>
+                            Credit card
+                        </Text>
+                    </Link>
+                    <Link to={routesPaths['Product']}>
+                        <Text className={styles.Header__link} color={stateTheme.color}>
+                            Product
+                        </Text>
+                    </Link>
+                    <Link to={routesPaths['Account']}>
+                        <Text className={styles.Header__link} color={stateTheme.color}>
+                            Account
+                        </Text>
+                    </Link>
+                    <Text className={styles.Header__link} color={stateTheme.color}>
+                        Resource
+                    </Text>
                 </Flex>
             </Container>
 
-            <Stack
-                direction="row"
-                spacing={4}
-                align="center"
-            >
-                <Button
-                    background="blue"
-                    colorText="white"
-                    className={styles.BtnSign}
+            <Flex gap={5} alignItems="center">
+                <span className={styles['Header__theme-block']} onClick={toggleTheme}>
+                    {currentColorMode === 'lightColorMode' ? (
+                        <SunIcon />
+                    ) : (
+                        <MoonIcon />
+                    )}
+                </span>
+
+                <Stack
+                    direction="row"
+                    spacing={4}
+                    align="center"
                 >
-                    Online bank
-                </Button>
-            </Stack>
-        </header >
+                    <Button
+                        background="blue"
+                        colorText="white"
+                        className={styles.BtnSign}
+                    >
+                        Online bank
+                    </Button>
+                </Stack>
+            </Flex>
+        </header>
     );
 };
--- src/containers/HeaderContainer/HeaderContainer.tsx
+++ src/containers/HeaderContainer/HeaderContainer.tsx
@@ -1,21 +1,45 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Header } from 'components';
+import { themeConfig, ThemeContext } from 'context';
+import { themeColorMode } from 'localStorage';
 
 interface IHeaderContainer {
     routesPaths: Record<string, string>;
 }
 
 export const HeaderContainer = ({ routesPaths }: IHeaderContainer) => {
+    const [currentColorMode, setCurrentColorMode] = React.useState<'lightColorMode' | 'darkColorMode'>('lightColorMode' || themeColorMode);
+
     const navigate = useNavigate();
+    const { stateTheme, setStateTheme } = React.useContext(ThemeContext);
 
     const goBackHome = () => {
         navigate(routesPaths['Home']);
     };
 
+    const toggleTheme = () => {
+        if (stateTheme === themeConfig.darkColorMode) {
+            setStateTheme(themeConfig.lightColorMode);
+            themeColorMode.setItem('lightColorMode');
+            setCurrentColorMode('lightColorMode');
+        } else {
+            setStateTheme(themeConfig.darkColorMode);
+            themeColorMode.setItem('darkColorMode');
+            setCurrentColorMode('darkColorMode');
+        }
+    };
+
     return (
         <>
-            <Header routesPaths={routesPaths} goBackHome={goBackHome} />
+            <Header
+                routesPaths={routesPaths}
+                goBackHome={goBackHome}
+                toggleTheme={toggleTheme}
+                stateTheme={stateTheme}
+                currentColorMode={currentColorMode}
+            />
         </>
     );
 };
