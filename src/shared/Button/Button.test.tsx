70caec081e0e5e2f111d495783f29b983f844066
polt0s <poltos.pro@gmail.com>
2022-09-05 17:58:55 +0300

    Added test for shared component Button

---
+++ src/shared/Button/Button.test.tsx
@@ -0,0 +1,60 @@
+import {
+    render,
+    screen,
+    fireEvent
+} from '@testing-library/react';
+
+import { Button } from './Button';
+
+describe('Button', () => {
+    const onClick = jest.fn();
+
+    it('Check Button is disable', () => {
+        render(
+            <Button disabled={true}>Send</Button>
+        );
+
+        const button = screen.getByRole('button');
+    
+        expect(button).toBeDisabled();
+    });
+
+    it('Check color and background props', () => {
+        render(
+            <Button
+                background="blue"
+                colorText="white"
+            >
+                    Online bank
+            </Button>
+        );
+
+        const button = screen.getByRole('button');
+
+        expect(button).toHaveClass('Background-blue');
+        expect(button).toHaveClass('Color-white');
+    });
+
+    it('Check width and height props', () => {
+        render(
+            <Button
+                width="10rem"
+                height="5rem"
+            >
+                Continue
+            </Button>
+        );
+
+        const button = screen.getByRole('button');
+
+        expect(button).toHaveStyle('width: 10rem');
+        expect(button).toHaveStyle('height: 5rem');
+    });
+
+    it('Check work onClick props', () => {
+        render(<Button onClick={onClick}>click me</Button>);
+
+        fireEvent.click(screen.getByText(/click me/i));
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+});
